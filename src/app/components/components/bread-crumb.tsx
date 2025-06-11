ba2118d54262ab73f8736072654f2d3d69d0749e
HeinThantZaw28 <heinthantzaw52@gmail.com>
2025-06-11 09:50:18 +0630

    [ENHANCEMENT]Bug fix npm build fail error

--- src/app/components/components/bread-crumb.tsx
+++ src/app/components/components/bread-crumb.tsx
@@ -1,37 +1,7 @@
 import React from "react";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
-import { ChevronRightIcon } from "lucide-react";
 
 const BreadCrumb = () => {
-  return (
-    <Breadcrumb>
-      <BreadcrumbList>
-        <BreadcrumbItem>
-          <BreadcrumbLink
-            href="#"
-            className="text-[#242424] hover:text-gray-400"
-          >
-            Switzerland Tours
-          </BreadcrumbLink>
-        </BreadcrumbItem>
-        <BreadcrumbSeparator>
-          <ChevronRightIcon className="h-4 w-4 text-[#242424]" />
-        </BreadcrumbSeparator>
-        <BreadcrumbItem>
-          <BreadcrumbPage className="text-[#242424]">
-            11D8N Switzerland Dream (Ap...
-          </BreadcrumbPage>
-        </BreadcrumbItem>
-      </BreadcrumbList>
-    </Breadcrumb>
-  );
+  return <div>BreadCrumb</div>;
 };
 
 export default BreadCrumb;
